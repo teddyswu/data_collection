8f0bdc00b183fb0266ed49bd92bc3a8ddaaa5f96
wu junlin <welcometeddy@yahoo.com.tw>
2015-11-10 12:15:23 +0800

    預載

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ Bundler.require(*Rails.groups)
 
 module DataCollection
   class Application < Rails::Application
+    config.autoload_paths += Dir["#{config.root}/lib/module/**/"] # 載入lib/module/底下所有.rb的檔案
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
